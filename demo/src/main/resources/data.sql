68c2936d90ff8ddb901796c790725af85f389c60
NoahBly <raz_b4@hotmail.com>
2023-06-10 13:28:20 +0200

    sql inject met profimage en bioinfo!

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO profiles (id,name) VALUES (1001,'henk');
+INSERT INTO profiles (id,name, profileimage, bioinformation) VALUES (1001,'henk','jantjesmitdownload.jpg', 'test');
 INSERT INTO users (id,username, password, email, profile_id) VALUES (1000,'henk', '$2a$12$0Oc6X4XBP8SCPd/eMkkG3eq9nZ8R3nz3P6h/0FnuZm.7/GNnL0XnK','test@testy.com',1001); -- password is 1234
 UPDATE profiles SET user_id=1000 WHERE id = 1001;
 
